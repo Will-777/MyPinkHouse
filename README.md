906a9f5dbc67d8bca0bfbf7e665e98e502d0d27d
Will-777 <axlsword@hotmail.com>
2017-08-26 15:08:35 +0900

    1s version temp of the ground

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # MyPinkHouse
 Raspberry Pi Minecraft Pink House codding. Let's try that ! ^^
+MCPI (where to begin) : https://www.raspberrypi.org/learning/getting-started-with-minecraft-pi/worksheet/
 
 ### source :
 Minecraft Tutorial: How To Make A Barbie Dream House!! Survival House (ASH#39)
 user : ADHDcraft
 URL : https://www.youtube.com/watch?v=oJo47QLOOX8 
+
+
 
 
--- myPinkHome.py
+++ myPinkHome.py
@@ -1,46 +1,36 @@
 """
 
+    MyPinkHome super programm !
+
+Requirement
+    Plateform : Raspberry Pi
+    Game : Minecraft Pi
+    OS : Raspbian
+    Python : 2.7.9
+
+    INPUT
+        Your list should con
+
+Date : 2017/8/26 
 
 
- MyPinkHome
+How to
+1) first get your current position . even if you move,
+    we're going to use that to localize where to construct
+    >>> pos = mc.player.getPos()
+    
+2) maybe you need some clean-up ? Use the bulldozer first !
+    >>> bulldozer(pos)
 
+3) make the ground of the house first
+    >>> ground(pos)
 
-INPUT
-Your list should con
-
- 
-
+4) The rest of the house, the roof, the internal decoration and the garden.
+    I am writting the code now so be patient :p
 
 """
-
-
 from mcpi.minecraft import Minecraft
 from mcpi import block
-
-
-
-
-bPink = 
-bPurple = 
-bWindow = block
-bStone = block.STONE.id
-bWater = block.WATER_STATIONARY.id
-
-# .data
-# wool = 35 
-# mc.setBlock(x, y, z, wool, 1)
-# 2: magenta, 3: Light Blue 4:Yellow
-# 
-# 6: Pink
-# 10: Purple
-# 
-
-
-
-
-# POINTING East, West, North or South ?!
-# torch can do.
-
 
 mc = Minecraft.create()
 
@@ -48,10 +38,89 @@ mc.postToChat("Hello everyone !")
 mc.postToChat("Let's try to build our Pink Home")
 mc.postToChat("in a single command line ! Can we ?")
 
+# !!! remember to DO the following command MANUALLY !!! (1)
+# pos = mc.player.getPos()
 
-myGround = [,]
 
-myWalls =
+# Define the block we are going to use.
+air = 0
+grass = block.GRASS.id
+wool = block.WOOL.id
+
+#color for wool blocks
+wPink = 6
+wPurple = 10
+
+# for windows
+wGlass = block.GLASS.id
+wGlass_Pane = block.GLASS_PANE.id
+# 
+
+bWater = block.WATER_STATIONARY.id
+
+# POINTING East, West, North or South ?!
+# torch can do.
+
+# My Var Might be here one day...
+# myGround = [,]
+# myWalls =
+
+
+def ground(pos):
+    """
+
+    """
+    mc.setBlocks(pos.x, pos.y-1,pos.z,pos.x+3, pos.y-1,pos.z, wool, wPurple)
+    
+    i = 1
+    #mc.setBlock(pos.x-1, pos.y-1,pos.z+1,wool, wPink)
+
+    while i <= 8:
+        mc.setBlock(pos.x-i, pos.y-1,pos.z+i,wool, wPurple)
+
+        mc.setBlocks(pos.x-i+1, pos.y-1, pos.z+i,
+                     pos.x-i+1+2+(2*i), pos.y-1,pos.z+i,
+                     wool, wPink)
+
+        mc.setBlock(pos.x-i+3+(2*i), pos.y-1,pos.z+i,
+                    wool, wPurple)
+        i += 1
+
+    # build the larger area
+    mc.setBlocks(pos.x-14, pos.y-1,pos.z+9,pos.x+17, pos.y-1,pos.z+27, wool, wPurple)
+    mc.setBlocks(pos.x-13, pos.y-1,pos.z+10,pos.x+16, pos.y-1,pos.z+26, wool, wPink)
+
+    # remove purple line in front
+    mc.setBlocks(pos.x-8, pos.y-1,pos.z+9,pos.x+11, pos.y-1,pos.z+9, wool, wPink)
+
+"""
+    ii = 1    
+    while ii <= 10:
+
+"""
+
+def makeTheHouse(pos):
+    pass
+
+def theRoof(pos):
+    pass
+
+def makeTheGarden(pos):
+    pass
+
+def makeTheDecoration(pos):
+    pass
+
+
+def bulldozer(pos):
+    # Make it empty
+    mc.setBlocks(pos.x-30, pos.y-1, pos.z-10,
+                 pos.x+40, pos.y+20, pos.z+40, air)
+
+    # put grass on the ground
+    mc.setBlocks(pos.x-30, pos.y-1, pos.z-10,
+                 pos.x+40, pos.y-1, pos.z+40, grass)
+    
 
 
 
@@ -67,11 +136,11 @@ def MCPI_Builder(myList):
         x,y,z(weight/High)
 
 
-    """
+    ##### Code from here?
+    pass
 
     # player position pos.x, pos.y & pos.z
     pos = mc.player.getPos()
-
 
     # building Start Point
     bSP = 
@@ -89,6 +158,8 @@ def MCPI_Builder(myList):
             mc.setBlocks(xa,ya,za,xb,yb,zb,material)
             
     return mc.postToChat("Work done !")
+    """
+    pass
 
 
 def MCPI_Build_The_Ground(myListForTheGround):
@@ -107,6 +178,6 @@ def MCPI_Build_The_Ground(myListForTheGround):
     myListForTheGround = [x,y,z,
     """
 
-    
+    pass    
     
     
